dd0214a36033aea0b9659efc2a9bb94cbe3b89ee
조우석 <20170628@eelab20.kaist.ac.kr>
2021-05-26 17:32:11 +0900

    added initial setup (conda.sh, setup_pip.sh)

---
+++ conda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# run with command : bash conda.sh
+
+conda create -n pj python=3.5.4
---
+++ setup_pip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run with command : bash setup_pip.sh
+# need Python 3.5.4
+
+pip install tensorflow==1.4.0
+pip install tensorflow-gpu==1.4.0
+pip install numpy==1.14.2
+pip install scipy
+pip install pillow
+pip install imageio
+pip install pretty_midi==0.2.8
+pip install pypianoroll==0.1.3
