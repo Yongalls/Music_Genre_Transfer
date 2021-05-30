0977a15527996dd7d56a485778b9b76b306766bd
Yongalls <lym7505@kaist.ac.kr>
2021-05-30 16:05:09 +0900

    Added converting clean midi to npy phrases

--- convert_clean.py
+++ convert_clean.py
@@ -1,15 +1,17 @@
 from __future__ import print_function
 import os
 import json
+import numpy as np
 import errno
 import pypianoroll
 from pypianoroll import Multitrack, Track
 import pretty_midi
 import shutil
 
-ROOT_PATH = './dataset/'
-converter_path = os.path.join(ROOT_PATH, 'MIDI/pop/pop_test/converter')
-cleaner_path = os.path.join(ROOT_PATH, 'MIDI/pop/pop_test/cleaner')
+ROOT_PATH = './MIDI/'
+data_path = os.path.join(ROOT_PATH, 'data')
+converted_path = os.path.join(ROOT_PATH, 'converted')
+LAST_BAR_MODE = 'remove'
 
 
 def make_sure_path_exists(path):
@@ -73,86 +75,74 @@ def midi_filter(midi_info, midi_name):
     return True
 
 
-def get_merged(multitrack):
+def get_merged(multitrack, midi_name):
     """Return a `pypianoroll.Multitrack` instance with piano-rolls merged to
     five tracks (Bass, Drums, Guitar, Piano and Strings)"""
-    category_list = {'Bass': [], 'Drums': [], 'Guitar': [], 'Piano': [], 'Strings': []}
-    program_dict = {'Piano': 0, 'Drums': 0, 'Guitar': 24, 'Bass': 32, 'Strings': 48}
+    category_list = {'Piano': [], 'Drums': []}
+    program_dict = {'Piano': 0, 'Drums': 0}
 
     for idx, track in enumerate(multitrack.tracks):
         if track.is_drum:
             category_list['Drums'].append(idx)
-        elif track.program//8 == 0:
+        else:
             category_list['Piano'].append(idx)
-        elif track.program//8 == 3:
-            category_list['Guitar'].append(idx)
-        elif track.program//8 == 4:
-            category_list['Bass'].append(idx)
-        else:
-            category_list['Strings'].append(idx)
 
-    tracks = []
-    for key in category_list:
-        if category_list[key]:
-            # print(category_list[key])
-            merged = multitrack[category_list[key]].get_merged_pianoroll()
-            tracks.append(Track(merged, program_dict[key], key == 'Drums', key))
-        else:
-            tracks.append(Track(None, program_dict[key], key == 'Drums', key))
-    return Multitrack(None, tracks, multitrack.tempo, multitrack.downbeat, multitrack.beat_resolution, multitrack.name)
+    merged = multitrack[category_list['Piano']].get_merged_pianoroll()
+    print("merged: ", merged.shape)
+    pr = get_bar_piano_roll(merged)
+    pr_clip = pr[:, :, 24:108]
+    if int(pr_clip.shape[0] % 4) != 0:
+        pr_clip = np.delete(pr_clip, np.s_[-int(pr_clip.shape[0] % 4):], axis=0)
+    pr_re = pr_clip.reshape(-1, 64, 84, 1)
+    print("changed: ", pr_re.shape)
+    return pr_re
 
+def get_bar_piano_roll(piano_roll):
+    if int(piano_roll.shape[0] % 64) is not 0:
+        if LAST_BAR_MODE == 'fill':
+            piano_roll = np.concatenate((piano_roll, np.zeros((64 - piano_roll.shape[0] % 64, 128))), axis=0)
+        elif LAST_BAR_MODE == 'remove':
+            piano_roll = np.delete(piano_roll,  np.s_[-int(piano_roll.shape[0] % 64):], axis=0)
+    piano_roll = piano_roll.reshape(-1, 64, 128)
+    return piano_roll
+    
 
 def converter(filepath):
     """Save a multi-track piano-roll converted from a MIDI file to target
     dataset directory and update MIDI information to `midi_dict`"""
     midi_name = os.path.splitext(os.path.basename(filepath))[0]
-    multitrack = Multitrack(beat_resolution=24, name=midi_name)
+    multitrack = Multitrack(beat_resolution=4, name=midi_name)
     pm = pretty_midi.PrettyMIDI(filepath)
     midi_info = get_midi_info(pm)
-    multitrack.parse_pretty_midi(pm)
-    #print(multitrack)
-    merged = get_merged(multitrack)
-
-    make_sure_path_exists(converter_path)
-    merged.save(os.path.join(converter_path, midi_name + '.npz'))
-
-    return [midi_name, midi_info]
+    if midi_filter(midi_info, midi_name):
+        multitrack.parse_pretty_midi(pm)
+        merged = get_merged(multitrack, midi_name)
+        return merged
+    else:
+        return None
 
 
 
 def main():
     """Main function of the converter"""
-    midi_paths = get_midi_path(os.path.join(ROOT_PATH, 'midi'))
-    midi_dict = {}
-    for midi_path in midi_paths:
-      print(midi_path)
-      
-    kv_pairs = [converter(midi_path) for midi_path in midi_paths]
-    for kv_pair in kv_pairs:
-        if kv_pair is not None:
-            midi_dict[kv_pair[0]] = kv_pair[1]
-
-    with open(os.path.join(ROOT_PATH, 'MIDI/pop/pop_test/midis.json'), 'w') as outfile:
-        json.dump(midi_dict, outfile)
-
-    print("[Done] {} files out of {} have been successfully converted".format(len(midi_dict), len(midi_paths)))
-
-    with open(os.path.join(ROOT_PATH, 'MIDI/pop/pop_test/midis.json')) as infile:
-        midi_dict = json.load(infile)
+    midi_paths = get_midi_path(data_path)
+    npys = [converter(midi_path) for midi_path in midi_paths]
     count = 0
-    make_sure_path_exists(cleaner_path)
-    midi_dict_clean = {}
-    for key in midi_dict:
-        if midi_filter(midi_dict[key], key):
-            midi_dict_clean[key] = midi_dict[key]
+    converted_list = []
+    for npy in npys:
+        if npy is not None:
             count += 1
-            shutil.copyfile(os.path.join(converter_path, key + '.npz'),
-                            os.path.join(cleaner_path, key + '.npz'))
+            converted_list.append(npy)
+    
+    total_npy = np.concatenate(converted_list, axis=0)
+    print("total npy: ", total_npy.shape)
 
-    with open(os.path.join(ROOT_PATH, 'MIDI/pop/pop_test/midis_clean.json'), 'w') as outfile:
-        json.dump(midi_dict_clean, outfile)
+    make_sure_path_exists(converted_path)
+    print("num of npy: ", total_npy.shape[0])
+    for i in range(total_npy.shape[0]):
+        np.save(os.path.join(converted_path, 'kpop_{}.npy'.format(i+1)), total_npy[i])
 
-    print("[Done] {} files out of {} have been successfully cleaned".format(count, len(midi_dict)))
+    print("[Done] {} files out of {} have been successfully cleaned".format(count, len(npys)))
 
 
 if __name__ == "__main__":
