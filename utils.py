ec929d980dc6a4ffe4f3ecd4df836a79dde5e741
wooseok <eric318@kaist.ac.kr>
2021-06-14 23:05:09 +0900

    modified utils.py

--- utils.py
+++ utils.py
@@ -98,8 +98,8 @@ def load_midi_data(midi_path):
 def load_npy_data(npy_data):
     npy_A = np.load(npy_data[0]) * 1.
     npy_B = np.load(npy_data[1]) * 1.
-    npy_AB = np.concatenate((npy_A.reshape(npy_A.shape[0], npy_A.shape[1], 1),
-                             npy_B.reshape(npy_B.shape[0], npy_B.shape[1], 1)), axis=2)
+    npy_AB = np.concatenate((npy_A.reshape(npy_A.shape[0], npy_A.shape[1], 2),
+                             npy_B.reshape(npy_B.shape[0], npy_B.shape[1], 2)), axis=2)
     return npy_AB
 # -----------------------------
 
